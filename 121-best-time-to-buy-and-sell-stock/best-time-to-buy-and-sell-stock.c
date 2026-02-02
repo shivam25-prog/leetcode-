e81538ebcdcdb52a53f242b9dc3f6704a8492875
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2026-02-02 21:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (82.62%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,25 @@
+int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize <= 1) {
+        return 0;
+    }
+    
+    int minPrice = prices[0];
+    int maxProfit = 0;
+    
+    for (int i = 1; i < pricesSize; i++) {
+        int profit = prices[i] - minPrice;
+        
+       
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+        
+       
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+    }
+    
+    return maxProfit;
+    
+}
