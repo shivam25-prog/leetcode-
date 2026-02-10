dde03359e4bde2b75a55838884dd27f07ec6c943
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2026-02-10 23:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (10.37%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    static int result[1000];
+    int freq[1001] = {0};
+    int k = 0;
+
+    for (int i = 0; i < nums1Size; i++) {
+        freq[nums1[i]]++;
+    }
+
+    for (int i = 0; i < nums2Size; i++) {
+        if (freq[nums2[i]] > 0) {
+            result[k++] = nums2[i];
+            freq[nums2[i]]--;
+        }
+    }
+
+    *returnSize = k;
+    return result;
+}
