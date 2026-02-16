ec5f8fa1766cf1dcd4121054d96a9395ac168855
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2026-02-16 08:56:32 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (20.95%) - LeetSync

---
+++ 189-rotate-array/rotate-array.c
@@ -0,0 +1,22 @@
+void reverse(int* nums, int left, int right)
+{
+    while(left < right)
+    {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k)
+{
+    if(numsSize == 0) return;
+
+    k = k % numsSize;
+
+    reverse(nums, 0, numsSize - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, numsSize - 1);
+}
