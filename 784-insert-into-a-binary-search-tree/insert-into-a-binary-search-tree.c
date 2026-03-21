9a7123a960bd5e93767c018a5a3d13afa90f6c9a
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2026-03-21 22:42:39 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (26.82%) - LeetSync

---
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    // Base case: empty spot found
+    if (root == NULL) {
+        return createNode(val);
+    }
+
+    // Traverse left or right depending on value
+    if (val < root->val) {
+        root->left = insertIntoBST(root->left, val);
+    } else {
+        root->right = insertIntoBST(root->right, val);
+    }
+
+    return root;
+}
